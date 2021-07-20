b00a26d3dbc0fd14f9c816baad473529ed0a017a
sohail <sohailqureshi82@gmail.com>
2021-07-21 01:03:38 +0530

    [sohail]Add: stock

--- StockAccountManagement/Program.cs
+++ StockAccountManagement/Program.cs
@@ -14,8 +14,8 @@ namespace StockAccountManagement
         ///    IBM      --     20.rs
         ///    Infosys  --     100.rs
         ///    TCS      --     50.rs
-        ///    Reliance --     40.rs
-        /// </summary>
+        ///    Reliance --     40.r
+        /// </summary> 
         /// <param name="args"></param>
         static void Main(string[] args)
         {
