97454e652567125dec8a4825c7de5580c0f2ecbd
sohail <sohailqureshi82@gmail.com>
2021-07-21 00:22:10 +0530

    [sohail] Refactor: to remove conflict

--- StockAccountManagement/Stock.cs
+++ StockAccountManagement/Stock.cs
@@ -12,21 +12,6 @@ namespace StockAccountManagement
     /// </summary>
     public class Stock
     {
-        [JsonProperty("stockname")]
-        public string Stockname { get; set; }
-        [JsonProperty("symbol")]
-        public string Symbol { get; set; }
-        [JsonProperty("stockprice")]
-        public int Stockprice { get; set; }
-        [JsonProperty("numberofstocks")]
-        public int Numberofstocks { get; set; }
-    }
-    /// <summary>
-    /// class to read the list of stocks from the file
-    /// </summary>
-    public class Root
-    {
-        [JsonProperty("stock account")]
-       public List<Stock> StockAccount { get; set; }
+        
     }
 }
--- StockAccountManagement/StockAccount.cs
+++ StockAccountManagement/StockAccount.cs
@@ -10,34 +10,7 @@ namespace StockAccountManagement
     public class StockAccount
     {
 
-        string filepath = @"C:\Users\Admin\Desktop\BridgeLabs Assignments\Stock Account Management\Stock-Account-Management\StockAccountManagement\StockAccountData.json";
-
-        public void GetStockDetails()
-        {
-            try
-            {
-                if (File.Exists(filepath))
-                {
-                    string stockData = File.ReadAllText(filepath);
-                    Root root = JsonConvert.DeserializeObject<Root>(stockData);
-                    List<Stock> stocks = root.StockAccount;
-                    foreach (Stock stock in stocks)
-                    {
-                        Console.WriteLine($"Stock Name: {stock.Stockname}\n Symbol: {stock.Symbol}\n Number of Stocks: {stock.Numberofstocks}\n Stock Price: {stock.Stockprice}");
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("File not found!!");
-                    throw new Exception();
-                }
-
-            }
-            catch(Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-        }
+       
     }
 }
 
